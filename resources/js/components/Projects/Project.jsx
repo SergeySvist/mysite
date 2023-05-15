6db7d50cd1d50900c754c15d931454bc4011cbae
Sergey Svist <sergeysvist557@gmail.com>
2023-05-15 23:39:40 +0300

    add: view details to Projects view

--- resources/js/components/Projects/Project.jsx
+++ resources/js/components/Projects/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProjectInfo from './ProjectInfo';
 
 const Project = ({project}) => {
     console.log(project.project_files_data[0].file_url);
@@ -6,6 +7,7 @@ const Project = ({project}) => {
         <div className='list-item'>
             <img src={project.project_files_data[0].file_url} alt="" width="100%"/>
             <p>{project.title}</p>
+            <ProjectInfo project={project}></ProjectInfo>
         </div>
     );
 }

a49f4dbae96f378859b7ec4d4a6ee34758232727
Sergey Svist <sergeysvist557@gmail.com>
2024-01-17 19:58:18 +0200

    fix: links in client project page

--- resources/js/client/components/Projects/ProjectInfo.jsx
+++ resources/js/client/components/Projects/ProjectInfo.jsx
@@ -21,11 +21,11 @@ const ProjectInfo = ({project}) => {
                 <div class="modal-body">
                     <img src={project.project_files_data[0].file_url} alt="" width="100%"/>
                     <p>{project.description}</p>
-                    <p>{project.link}</p>
+                    <a>{project.link}</a>
                 </div>
                 </div>
             </div>
-            </div>        
+            </div>
         </div>
     );
 }

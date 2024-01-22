b0c4e6f8f7762b1ae61fa6bff4cc95d99b401554
Sergey Svist <sergeysvist557@gmail.com>
2024-01-22 09:49:59 +0200

    fix skill list colours

--- resources/js/client/components/Skills/Skill.jsx
+++ resources/js/client/components/Skills/Skill.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Skill = ({skill}) => {
     return (
         <div className='skill'>
-            <div class="circle" style={{backgroundImage: `conic-gradient(#304d86 ${skill.rate}%, #8ca0c9 0)`}}>
+            <div class="circle" style={{backgroundImage: `conic-gradient(#8ca0c9 ${skill.rate}%, #304d86 0)`}}>
                 <div class="inner">{skill.title}</div>
             </div>
         </div>

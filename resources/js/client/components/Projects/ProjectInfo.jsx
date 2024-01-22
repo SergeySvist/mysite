9172b47541026751985c2c4b68b886f9eeb71243
Sergey Svist <sergeysvist557@gmail.com>
2024-01-22 09:57:53 +0200

    fix project modal button

--- resources/js/client/components/Projects/ProjectInfo.jsx
+++ resources/js/client/components/Projects/ProjectInfo.jsx
@@ -22,7 +22,7 @@ const ProjectInfo = ({project}) => {
                     <img src={project.project_files_data[0].file_url} alt="" width="100%"/>
                     <p>{project.description}</p>
                     <br />
-                    <a href={project.link} className="code-link-button">Link to the project code</a>
+                    <a href={project.link} className='btn btn-primary'>Link to the project code</a>
                 </div>
                 </div>
             </div>

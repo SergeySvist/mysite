b4c54a44513ba375855984e7ca8fcf5e0716075c
Sergey Svist <sergeysvist557@gmail.com>
2024-01-23 11:43:32 +0200

    add: google analitics tag

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Control Panel</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="https://sergeysvist.com/storage/images/favicon.ico">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3S0WRQ8QXP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3S0WRQ8QXP');
+    </script>
+
     @vite('resources/css/admin.css')
 </head>
 <body>

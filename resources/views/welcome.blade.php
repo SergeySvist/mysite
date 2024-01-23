46af3cda84496c599429dc177b2beeebd4dd9440
Sergey Svist <sergeysvist557@gmail.com>
2024-01-23 11:39:25 +0200

    add: google analitics tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sergey Svist</title>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3S0WRQ8QXP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3S0WRQ8QXP');
+    </script>
     @vite('resources/css/app.css')
 </head>
 <body>
